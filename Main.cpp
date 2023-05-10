f7461460dca2969e393a47dfa2b5d33b95117fdc
Himateju V <90406478+HimaTeju@users.noreply.github.com>
2023-05-10 09:02:00 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -6,6 +6,86 @@
 
 using namespace std;
 
+
+
+void addData()
+{
+
+    system("cls");
+
+    fstream file("data.csv",ios::out | ios::app);
+
+    if(!file)
+    {
+        cout<<"\nFailed to open data.csv / data.csv file not found";
+    }
+
+    else
+     cout<<"\t|Enter Data|"<<endl<<endl;
+
+     string roll,name,college,branch;
+     string moreData;
+
+     cout<<"\nEnter Student's ROll Number: ";
+     cin>>roll;
+     file<<"\n"<<roll<<",";
+
+     cout<<"\nEnter Student's Name: ";
+     cin>>name;
+     file<<name<<",";
+
+     cout<<"\nEnter College: ";
+     cin>>college;
+     file<<college<<",";
+
+     cout<<"\nEnter Branch: ";
+     cin>>branch;
+     file<<branch<<"\n";
+
+     file.close();
+
+     cout<<"\nAdd more Data? (Y/N)";
+
+     cin>>moreData;
+     if(moreData=="Y" || moreData=="y")
+     {
+        addData();
+     }
+
+     else
+      return;     
+}
+
+void viewData(){
+
+    system("cls");
+
+    fstream file("data.csv");
+
+    if(!file)
+    {
+        cout<<"Fail in opening this file";
+    }
+    cout<<"\n\t\t\t|Students Record\n\n";
+    
+    cout<<"Roll \t Name \t\t\t College \t\t Branch\n";
+
+    string roll,name,college,branch;
+
+    while(!file.eof()){
+
+        getline(file,roll,',');
+        getline(file,name,',');
+        getline(file,college,',');
+        getline(file,branch,'\n');
+
+        cout<<roll<<" \t "<<name<<" \t\t\t "<<college<<" \t\t "<<branch<<endl;
+        cout<<endl;   
+    }
+    file.close();
+}
+
+
 void studentFunction(){
 
     system("cls");
